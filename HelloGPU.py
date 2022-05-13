8826e5d8115353ce53564b40606ccc72e5186bbd
summer <h262sawamura@i.softbank.jp>
2022-05-13 10:39:13 +0900

    Changed for-loop simple

--- HelloGPU.py
+++ HelloGPU.py
@@ -4,9 +4,9 @@
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
-    for k in range(len(physical_devices)):
-        tf.config.experimental.set_memory_growth(physical_devices[k], True)
-        print('memory growth:', tf.config.experimental.get_memory_growth(physical_devices[k]))
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+        print('memory growth:', tf.config.experimental.get_memory_growth(device))
 else:
     print("Not enough GPU hardware devices available")
 
